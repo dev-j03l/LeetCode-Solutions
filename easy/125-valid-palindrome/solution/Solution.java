8d63118fad2bce762edac5783d2987c87b6b0981
dev-j03l <jojanj@tcd.ie>
2025-07-03 22:32:57 +0100

    My solution for valid palindrome

---
+++ easy/125-valid-palindrome/solution/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        int j = s.length() - 1;
+        int i = 0;
+        s = s.toLowerCase();
+
+        while(i <= j){
+            while(i < j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            while(i < j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
