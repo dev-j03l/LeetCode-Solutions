9a3c11e6d73fa43a03d6e667bd668984e7499c6c
Joel Mathew Jojan <jojanj@tcd.ie>
2025-07-29 07:35:05 +0100

    Time: 2 ms (62.12%) | Memory: 45.5 MB (59.58%) - LeetSync

---
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    boolean isInAsc(int[] nums, int l, int r){
+        for(int i = l; i < r; i++){
+            if(nums[i] + 1 != nums[i+1]) return false;
+        }
+        return true;
+    }
+    public int[] resultsArray(int[] nums, int k) {
+        if(nums.length ==  1) return nums;
+
+        int  l = 0, r = k -1;
+        int arr[] = new int[nums.length - k + 1];
+        while(r < nums.length){
+            if(isInAsc(nums, l , r)) arr[l] = nums[r];
+            else{
+            arr[l] = -1;
+            }
+            l++;
+            r++;
+        }
+        return arr;
+    }
+}
