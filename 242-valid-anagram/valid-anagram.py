aa27f4bf492b600cde5d5f740d8f2e3b62b5c8cf
Joel Mathew Jojan <jojanj@tcd.ie>
2025-08-10 09:30:30 +0100

    Time: 11 ms (72.29%) | Memory: 18 MB (34.01%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        # return sorted(s) == sorted(t) sorting #approach
+        letters = defaultdict(int)
+        for x in s:
+            letters[x] += 1
+        
+        for x in t:
+            letters[x] -= 1
+        
+        for x in letters.values():
+            if x != 0:
+                return False
+        
+        return True
