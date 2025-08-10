c661ec213249e119a9436c5e042f5848533a7651
Joel Mathew Jojan <jojanj@tcd.ie>
2025-08-10 12:55:10 +0100

    Time: 13 ms (6.71%) | Memory: 20.8 MB (5.21%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_indices = {}
+        for i, letter in enumerate(s):
+            if letter not in s_indices:
+                s_indices[letter] = [i]
+            else:
+                s_indices[letter].append(i)
+        
+        t_indices = {}
+        for i, letter in enumerate(t):
+            if letter not in t_indices:
+                t_indices[letter] = [i]
+            else:
+                t_indices[letter].append(i)
+        
+        print(t_indices.values())
+        print(s_indices.values())
+        return list(t_indices.values()) == list(s_indices.values())
