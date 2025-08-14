e936c5146ca33b9ea76be7c07710ea6779012de2
Joel Mathew Jojan <jojanj@tcd.ie>
2025-08-14 10:48:55 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (89.34%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -1,6 +1,5 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        if len(students) == 0: return 0
         student_q = deque()
         # Add all our students to the queue
         for student in students:
@@ -9,7 +8,7 @@ class Solution:
         size_q = len(students)
         top = 0
 
-        while student_q and size_q:
+        while size_q:
             first = student_q.popleft()
             if first == sandwiches[top]:
                 top += 1
@@ -17,5 +16,6 @@ class Solution:
             else:
                 size_q -= 1
                 student_q.append(first)
+
         return len(student_q)
         
