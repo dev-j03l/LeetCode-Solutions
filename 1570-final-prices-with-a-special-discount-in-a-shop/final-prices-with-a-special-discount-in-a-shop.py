4dc143e60dfb19e179aa34120d28c92608f110a0
Joel Mathew Jojan <jojanj@tcd.ie>
2025-08-21 13:27:44 +0100

    Time: 0 ms (100.00%) | Memory: 17.8 MB (49.09%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,9 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = []
+        for i, p in enumerate(prices):
+            while stack and p <= prices[stack[-1]]:
+                prices[stack.pop()] -= p
+            stack.append(i)
+        
+        return prices
