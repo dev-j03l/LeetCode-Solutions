568a513c57bcaff9d2587624abe2097e343ca18c
Joel Mathew Jojan <jojanj@tcd.ie>
2025-09-24 18:52:48 +0100

    Time: 0 ms (100.00%) | Memory: 17.8 MB (49.61%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        letter_to_row = {}
+        for l in "qwertyuiop":
+            letter_to_row[l] = 1
+        
+        for l in "asdfghjkl":
+            letter_to_row[l] = 2
+        
+        for l in "zxcvbnm":
+            letter_to_row[l] = 3
+        
+        res = []
+        for word in words:
+            res.append(word)
+            print(res)
+            for i in range(1, len(word)):
+                if letter_to_row[word[i].lower()] != letter_to_row[word[i-1].lower()]:
+                    res.pop()
+                    break
+        
+        return res
+
+
